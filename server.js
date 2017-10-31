ad5be9e4e292fb1c80b0effe94c08f95712d350d
sbrycebarker <sbrycesti@gmail.com>
2017-10-30 19:43:41 -0600

    setup

--- server.js
+++ server.js
@@ -1,2 +1,33 @@
 const express = require('express'),
-      cors =
+      session = require('express-session'),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
+      massive = require('massive'),
+      Auth0 = require('passport-auth0'),
+      config = require('./config.js');
+      elephant = "string"
+
+
+      const app = express();
+
+
+      // <<===============================SERVER SETUP=============================>>
+
+      app.use(bodyParser.json())
+      app.use(cors())
+      app.use(session({
+        resave: true,
+        saveUninitialized: true,
+        secret: 'pizza'
+      }))
+
+      app.use(passport.initialize());
+      app.use(passport.session())
+
+      app.use(express.static('./public'))
+
+      var port = 8050
+
+      app.listen(port, function(){
+        console.log("listining on port" + port)
+      })
