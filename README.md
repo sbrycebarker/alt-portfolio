b8800dd3bc6272027ccd9c2245a9743a126d619b
sbrycebarker <sbrycesti@gmail.com>
2017-11-03 12:10:59 -0600

    move

--- README.md
+++
@@ -1,5 +0,0 @@
-# alt-portfolio
-
-*******************************************************************************************
-
-This is a revamp of my portfolio site for an alternate look that may be more attactive and fitting to modern website themes.
--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-const express = require('express'),
-      session = require('express-session'),
-      bodyParser = require('body-parser'),
-      cors = require('cors'),
-      massive = require('massive'),
-      passport = require('passport')
-      Auth0 = require('passport-auth0'),
-      config = require('./config.js');
-      elephant = "string"
-
-
-      const app = express();
-
-
-      // <<===============================SERVER SETUP=============================>>
-
-      app.use(bodyParser.json())
-      app.use(cors())
-      app.use(session({
-        resave: true,
-        saveUninitialized: true,
-        secret: 'pizza'
-      }))
-
-      app.use(passport.initialize());
-      app.use(passport.session())
-
-      app.use(express.static('./public'))
-
-      var port = 8050
-
-      app.listen(port, function(){
-        console.log("listining on port" + port)
-      })
+// const express = require('express'),
+//       session = require('express-session'),
+//       bodyParser = require('body-parser'),
+//       cors = require('cors'),
+//       massive = require('massive'),
+//       passport = require('passport')
+//       Auth0 = require('passport-auth0'),
+//       config = require('./config.js');
+//       elephant = "string"
+//
+//
+//       const app = express();
+//
+//
+//       // <<===============================SERVER SETUP=============================>>
+//
+//       app.use(bodyParser.json())
+//       app.use(cors())
+//       app.use(session({
+//         resave: true,
+//         saveUninitialized: true,
+//         secret: 'pizza'
+//       }))
+//
+//       app.use(passport.initialize());
+//       app.use(passport.session())
+//
+//       app.use(express.static('./public'))
+//
+//       var port = 8050
+//
+//       app.listen(port, function(){
+//         console.log("listining on port" + port)
+//       })
